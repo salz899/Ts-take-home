aec864bc122f86c9524825f173993621f78ce565
salz899 <68082666+salz899@users.noreply.github.com>
2020-12-01 09:03:57 -0600

    Add files via upload

---
+++ main.js
@@ -0,0 +1,8 @@
+
+// JS function to show form inputs in an alert
+
+function showAlert() {
+    var a =  document.getElementById('email').value;
+    var b = document.getElementById('name').value;
+    alert("Email: "+a+"       "+"\nName: "+b)
+}
