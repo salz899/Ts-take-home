738d4fa64363b2d6845e3b227c3c4298ab607bda
salz899 <68082666+salz899@users.noreply.github.com>
2020-12-01 09:05:42 -0600

    Delete main.js

--- ts-web-dev-take-home/main.js
+++
@@ -1,8 +0,0 @@
-
-// JS function to show form inputs in an alert
-
-function showAlert() {
-    var a =  document.getElementById('email').value;
-    var b = document.getElementById('name').value;
-    alert("Email: "+a+"       "+"\nName: "+b)
-}
